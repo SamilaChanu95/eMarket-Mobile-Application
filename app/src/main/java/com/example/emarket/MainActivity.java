8bf71e6220b2dc04359badbc1c533268b82a7ad2
SamilaChanu95 <chanukasamila@gmail.com>
2019-11-05 21:39:20 +0530

    Initialize the register and login buttons.

--- app/src/main/java/com/example/emarket/MainActivity.java
+++ app/src/main/java/com/example/emarket/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.emarket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button registerButton,loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.main_login_btn);
+        registerButton = (Button) findViewById(R.id.main_register_btn);
+
     }
 }
