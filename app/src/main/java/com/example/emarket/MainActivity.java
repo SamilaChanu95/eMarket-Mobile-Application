7b4208e86cc3f9b8b7ee2684ec029dde8dafe097
SamilaChanu95 <chanukasamila@gmail.com>
2019-11-07 08:05:42 +0530

    Set the on click method for change the Login page.

--- app/src/main/java/com/example/emarket/MainActivity.java
+++ app/src/main/java/com/example/emarket/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.emarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
 
         loginButton = (Button) findViewById(R.id.main_login_btn);
         registerButton = (Button) findViewById(R.id.main_register_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
